322808ca698759d87a661da771beca6fb2c310b8
Anders Poulsen <s204423@student.dtu.dk>
2022-03-04 10:54:59 +0100

    Added types and productions

--- Fm4FunTypesAST.fs
+++ Fm4FunTypesAST.fs
@@ -2,12 +2,40 @@
 // to store represent arithmetic expressions
 module Fm4FunTypesAST
 
-type expr =
+type Variable = String
+type Array = Aexpr List
+and Aexpr =
   | Num of float
-  | MultExpr of (expr * expr)
-  | DivExpr of (expr * expr)
-  | AddExpr of (expr * expr)
-  | MinusExpr of (expr * expr)
-  | PowExpr of (expr * expr)
-  | UPlusExpr of (expr)
-  | UMinusExpr of (expr)
+  | Var of Variable
+  | ArrEntry of (Array*Aexpr)
+  | MultExpr of (Aexpr * Aexpr)
+  | DivExpr of (Aexpr * Aexpr)
+  | AddExpr of (Aexpr * Aexpr)
+  | MinusExpr of (Aexpr * Aexpr)
+  | PowExpr of (Aexpr * Aexpr)
+  | UMinusExpr of (Aexpr)
+
+type Bexpr =
+  | B of bool
+  | AndExpr of (Bexpr*Bexpr)
+  | OrExpr of (Bexpr*Bexpr)
+  | ScAndExpr of (Bexpr*Bexpr)
+  | ScOrExpr of (Bexpr*Bexpr)
+  | NotExpr of Bexpr
+  | EqualExpr of (Aexpr*Aexpr)
+  | NotEqualExpr of (Aexpr*Aexpr)
+  | GreaterThanExpr of (Aexpr*Aexpr)
+  | GreaterOrEqualExpr of (Aexpr*Aexpr)
+  | LessThanExpr of (Aexpr*Aexpr)
+  | LessOrEqualExpr of (Aexpr*Aexpr)
+  
+type Cexpr =
+  | AssignExpr of (Variable*Aexpr)
+  | AssignToArrExpr of (Array*Aexpr*Aexpr)
+  | SkipExpr
+  | AlsoExpr of (Cexpr*Cexpr)
+  | IfExpr of GuardedExpr
+  | DoExpr of GuardedExpr
+and GuardedExpr =
+  | ArrowExpr of (Bexpr*Cexpr)
+  | AlsoExpr of (GuardedExpr*GuardedExpr)
