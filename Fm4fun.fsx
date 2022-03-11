edfa3e9d79bc0ee19fe9afaddd59a7d86a913127
Dyberg <mathias.rerup-dyberg@hotmail.com>
2022-03-11 09:27:34 +0100

    Temp

--- Fm4fun.fsx
+++ Fm4fun.fsx
@@ -123,7 +123,7 @@ let rec compute n =
         // We parse the input string
         let e = parse (Console.ReadLine())
         // and print the result of evaluating it
-        printfn "Result: %s" (evalC(e))
+        printfn "Result: %s" (evalGC(e))
         compute n
         with err -> compute (n-1)
 
